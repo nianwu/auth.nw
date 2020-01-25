ea35999c208125db9098bc85f404446e89a65abf
nianwu <31959790+nianwu@users.noreply.github.com>
2020-01-25 18:04:00 +0800

    fix zhe config fron identity

--- identity_server_4/Config.cs
+++ identity_server_4/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -11,17 +12,59 @@ namespace identity_server_4
     {
         public static IEnumerable<IdentityResource> Ids =>
             new IdentityResource[]
-            { 
-                new IdentityResources.OpenId()
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResources.Phone(),
+                new IdentityResources.Address(),
             };
 
         public static IEnumerable<ApiResource> Apis =>
-            new ApiResource[] 
-            { };
-        
-        public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
-        
+            new ApiResource[]
+            {
+                new ApiResource("api1", "My API")
+            };
+
+        public static IEnumerable<Client> Clients => new Client[]
+        {
+            new Client
+            {
+                ClientId = "client",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "api1" }
+            },
+            // JavaScript Client
+            new Client
+            {
+                ClientId = "js",
+                ClientName = "JavaScript Client",
+                AllowedGrantTypes = GrantTypes.Code,
+                RequirePkce = true,
+                RequireClientSecret = false,
+
+                RedirectUris =           { "http://localhost:5003/callback.html" },
+                PostLogoutRedirectUris = { "http://localhost:5003/index.html" },
+                AllowedCorsOrigins =     { "http://localhost:5003" },
+
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "api1"
+                }
+            }
+        };
     }
+
 }
