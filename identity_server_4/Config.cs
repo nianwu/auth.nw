c1ba3f5da2f365d96c485072f7f7a07f60e84251
nianwu <31959790+nianwu@users.noreply.github.com>
2020-01-25 20:36:06 +0800

    1. 增加测试用户 2. 修复 is run 脚本错误

--- identity_server_4/Config.cs
+++ identity_server_4/Config.cs
@@ -1,9 +1,11 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+﻿using System.Security.Claims;
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
 using IdentityServer4;
 using IdentityServer4.Models;
+using IdentityServer4.Test;
 using System.Collections.Generic;
 
 namespace identity_server_4
@@ -20,11 +22,10 @@ namespace identity_server_4
                 new IdentityResources.Address(),
             };
 
-        public static IEnumerable<ApiResource> Apis =>
-            new ApiResource[]
-            {
-                new ApiResource("api1", "My API")
-            };
+        public static IEnumerable<ApiResource> Apis => new ApiResource[]
+        {
+            new ApiResource("api1", "My API")
+        };
 
         public static IEnumerable<Client> Clients => new Client[]
         {
@@ -65,6 +66,18 @@ namespace identity_server_4
                 }
             }
         };
-    }
 
+        public static IEnumerable<TestUser> TestUsers => new TestUser[]{
+            new TestUser{
+                SubjectId = "yuanzhiyuan",
+                Username = "yuanzhiyuan",
+                Password = "yuanzhiyuan",
+                Claims = new Claim[]{
+                    new Claim(ClaimTypes.Name , "袁智远"),
+                    new Claim(ClaimTypes.MobilePhone, "13331070864"),
+                    new Claim(ClaimTypes.Email , "1262163090@qq.com")
+                }
+            }
+        };
+    }
 }
